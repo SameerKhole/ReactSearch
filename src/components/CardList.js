b7b5a6565faa9b6a5f8e7a3a74f4e385d356613a
SameerKhole <kholesameer110@gmail.com>
2018-04-16 10:34:03 +0530

    Suitable file structure for React

---
+++ src/components/CardList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) =>{
+	const cardsArray = robots.map((user,i )=> {
+		return (<Card 
+					key = {i} 
+					id={robots[i].id} 
+					name={robots[i].name} 
+					email={robots[i].email}
+				/> 
+				);
+	})
+	return (
+		<div>
+			{cardsArray}
+		</div>
+	);
+}
+
+// Instead of using a separate variable we can directly use { } this 
+// because anything written in the braces is javascript
+
+// const CardList = ({robots}) =>{
+// 	return (
+// 		<div>
+// 			{
+// 				robots.map((user,i )=> {
+// 					return (
+// 							<Card 
+// 								key = {i} 
+// 								id={robots[i].id} 
+// 								name={robots[i].name} 
+// 								email={robots[i].email}
+// 							/> 
+// 					);
+// 				})
+// 			}
+// 		</div>
+// 	);
+// }
+
+
+export default CardList;
